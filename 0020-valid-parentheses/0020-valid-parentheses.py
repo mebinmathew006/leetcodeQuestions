002cdd79335b4f4fe1a6cd349fbcc803f2c0faaa
Mebin Mathew <mebinmathew006@gmail.com>
2025-09-03 16:00:45 +0530

    Time: 4 ms (9.45%), Space: 18 MB (32.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,12 +3,12 @@ class Solution:
         stack = []
         maps={'}':'{',']':'[',')':'('}
         for i in s:
-            if i in maps.values():
+            if i in maps.values():  # opening brackets
                 stack.append(i)
-            elif i in maps :
-                if not stack and  stack[-1]!=maps[i]:
-                    return False
-                stack.pop(-1)
+            elif i in maps:  # closing brackets
+                if not stack or stack[-1] != maps[i]:
+                    return False   # mismatch or empty stack
+                stack.pop()
 
         return len(stack)==0
                 
