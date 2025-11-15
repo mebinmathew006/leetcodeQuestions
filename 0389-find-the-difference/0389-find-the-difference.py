7959e8fcb0701a35ad54b59e1c1ce56b7bdd9579
Mebin Mathew <mebinmathew006@gmail.com>
2025-11-15 10:23:44 +0530

    Time: 3 ms (52.77%), Space: 18 MB (24.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,22 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-       for i in t:
-        if t.count(i) != s.count(i):
-            return i
-        
+        di={}
+        for i in s:
+            if i in di:
+                di[i]+=1
+            else:
+                di[i]=1
+        dii={}
+        for i in t:
+            if i in dii:
+                dii[i]+=1
+            else:
+                dii[i]=1
+        for i in t:
+            try:
+                if dii[i]:
+                    pass
+                if di[i] != dii[i]:
+                    return i
+            except Exception as e:
+                return i
